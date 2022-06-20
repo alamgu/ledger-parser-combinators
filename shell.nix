c8687cf603e71c5151e8d81eff041d791c54125c
Rosario Pulella <RosarioPulella@gmail.com>
2022-06-20 09:23:27 -0400

    Add shell.nix that properly extens ledger-platform

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> {}
 }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    cargo
-    rustc
-    cmake
-    protobuf
-  ];
-
+let
+  ledger-platform = import ./dep/ledger-platform {};
+in
+ledger-platform.rustShell.overrideAttrs(old: {
+  shellHook = (old.shellHook or "") + ''
+     export PATH=${pkgs.protobuf}/bin:$PATH
+  '';
+  
   PROTO_INCLUDE = "${pkgs.protobuf}/include";
-}
+})
